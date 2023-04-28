7ca0f1c81b329d9f946e61393e34fb312882ac05
Roman0nyshchuk <126744790+Roman0nyshchuk@users.noreply.github.com>
2023-04-27 22:05:00 -0400

    CucumberBDDFinalProject

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/feature",
+        features = "src/test/resources/features/",
         glue = "stepDefinitions",
         tags = "@Smoke_Suite",
         dryRun = false,
